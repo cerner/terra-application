e507f911b60d98bd444a909354377d598d9fa983
Emily Rohrbough <emily.rohrbough@cerner.com>
2019-11-20 16:40:48 -0600

    wdio tests

--- tests/wdio/modal-manager/modal-manager-spec.js
+++ tests/wdio/modal-manager/modal-manager-spec.js
@@ -1,47 +1,84 @@
-// const selector = '#root';
+const selector = '#root';
 
-// Terra.describeViewports('ModalManager', ['large'], () => {
-//   before(() => browser.url('/#/raw/tests/terra-application/modal-manager/navigation-prompt'));
-//   it('opens the slide-panel', () => {
-//     browser.click('#root-component .disclose-large');
-//     browser.waitForVisible('[class*="slide-group"] #DemoContainer-1 .disclose-tiny');
-//     Terra.validates.element({ selector });
-//   });
+Terra.describeViewports('ModalManager', ['large'], () => {
+  before(() => browser.url('/#/raw/tests/terra-application/modal-manager/navigation-prompt'));
 
-//   it('marks panel as having a pending action', () => {
-//     browser.click('[class*="slide-group"] #DemoContainer-1 .pending-action-toggle');
-//   });
+  it('opens the modal', () => {
+    browser.click('#root-component .disclose-large');
+    browser.waitForVisible('[class*="slide-group"] #DemoContainer-1 .disclose-tiny');
+    Terra.validates.element('1. root modal', { selector });
+  });
 
-//   it('displays navigation prompt when attempting to close the panel', () => {
-//     browser.click('[class*="slide-group"] #DemoContainer-1 .disclose-tiny');
-//     browser.waitForVisible('[class*="notification-dialog"]');
-//     Terra.validates.element('navigation prompt', { selector });
-//   });
+  it('marks modal as having a pending action', () => {
+    browser.click('[class*="slide-group"] #DemoContainer-1 .pending-action-toggle');
+  });
 
-//   it('it rejects the closure and keeps the panel open', () => {
-//     browser.click('[class*="notification-dialog"] [class*="actions"] button:nth-child(2)');
-//     browser.waitForVisible('[class*="notification-dialog"]', 1000, false);
-//     Terra.validates.element('closure rejected', { selector });
-//   });
+  it('displays navigation prompt when attempting to close the modal', () => {
+    browser.click('[class*="Modal"] [class*="ActionHeader"] .header-close-button');
+    browser.waitForVisible('[class*="notification-dialog"]');
+    Terra.validates.element('2. root modal-on close-navigation prompt', { selector });
+  });
 
-//   it('opens a nested slide-panel', () => {
-//     browser.click('[class*="slide-group"] #DemoContainer-1 .disclose-tiny');
-//     browser.waitForVisible('[class*="slide-group"] #DemoContainer-2 .disclose-tiny');
-//     Terra.validates.element('nested disclosure', { selector });
-//   });
+  it('it rejects the closure and keeps the modal open', () => {
+    browser.click('[class*="notification-dialog"] [class*="actions"] button:nth-child(2)');
+    expect(browser.isExisting('[class*="notification-dialog"]')).to.be.false;
+    browser.waitForVisible('[class*="slide-group"] #DemoContainer-1 .disclose-tiny');
+    Terra.validates.element('3. root modal-on close-rejected prompt', { selector });
+  });
 
-//   it('marks panel as having a pending action', () => {
-//     browser.click('[class*="slide-group"] #DemoContainer-2 .pending-action-toggle');
-//   });
+  it('opens a nested modal', () => {
+    browser.click('[class*="slide-group"] #DemoContainer-1 .disclose-tiny');
+    browser.waitForVisible('[class*="slide-group"] #DemoContainer-2 .disclose-tiny');
+    Terra.validates.element('4. nested modal', { selector });
+  });
 
-//   it('opens a 2nd nested slide-panel', () => {
-//     browser.click('[class*="slide-group"] #DemoContainer-1 .disclose-tiny');
-//     browser.waitForVisible('[class*="slide-group"] #DemoContainer-2 .disclose-tiny');
-//   });
+  it('marks modal as having a pending action', () => {
+    browser.click('[class*="slide-group"] #DemoContainer-2 .pending-action-toggle');
+  });
 
-//   it('does not display a navigation prompt when attempting to go back to the nested panel', () => {
-//     browser.click('[class*="slide-group"] #DemoContainer-1 .disclose-tiny');
-//     browser.waitForVisible('[class*="notification-dialog"]', 1000, false);
-//     expect()
-//   });
-// });
+  it('opens a 2nd nested modal', () => {
+    browser.click('[class*="slide-group"] #DemoContainer-2 .disclose-tiny');
+    browser.waitForVisible('[class*="slide-group"] #DemoContainer-3 .disclose-tiny');
+  });
+
+  it('does not display a navigation prompt when attempting to go back to the nested modal', () => {
+    browser.click('[class*="Modal"] [class*="ActionHeader"] .header-back-button');
+    expect(browser.isExisting('[class*="notification-dialog"]')).to.be.false;
+    browser.waitForVisible('[class*="slide-group"] #DemoContainer-2 .disclose-tiny');
+  });
+
+  it('displays a navigation prompt when attempting to go back to the root modal', () => {
+    browser.click('[class*="Modal"] [class*="ActionHeader"] .header-back-button');
+    browser.waitForVisible('[class*="notification-dialog"]');
+    Terra.validates.element('5. nested modal-on back-navigation-prompt', { selector });
+  });
+
+  it('rejects the closure and keeps the modal open', () => {
+    browser.click('[class*="notification-dialog"] [class*="actions"] button:nth-child(2)');
+    expect(browser.isExisting('[class*="notification-dialog"]')).to.be.false;
+    browser.waitForVisible('[class*="slide-group"] #DemoContainer-2 .disclose-tiny');
+    Terra.validates.element('6. nested modal-on back-rejected prompt', { selector });
+  });
+
+  it('displays navigation prompt when attempting to close the modal', () => {
+    browser.click('[class*="Modal"] [class*="ActionHeader"] .header-close-button');
+    browser.waitForVisible('[class*="notification-dialog"]');
+    Terra.validates.element('7. nest modal-on close-navigation prompt', { selector });
+  });
+
+  it('accepts the go back and displays the previous modal', () => {
+    browser.click('[class*="notification-dialog"] [class*="actions"] button:nth-child(1)');
+    browser.waitForVisible('[class*="slide-group"] #DemoContainer-1 .disclose-tiny');
+  });
+
+  it('keeps the navigation prompt open since the root modal also has a pending action', () => {
+    expect(browser.isExisting('[class*="notification-dialog"]')).to.be.true;
+    Terra.validates.element('8. root modal-on close-navigation prompt', { selector });
+  });
+
+  it('successfully closes the modal', () => {
+    browser.click('[class*="notification-dialog"] [class*="actions"] button:nth-child(1)');
+    expect(browser.isExisting('[class*="slide-group"] #DemoContainer-1 .disclose-tiny')).to.be.false;
+    Terra.validates.element('9. modal-closed', { selector });
+  });
+});

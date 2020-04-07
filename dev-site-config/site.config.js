5e36683414f828e4a187e43bbf06c75c8984e509
Matt Henkes <mjhenkes@gmail.com>
2020-04-07 14:39:41 -0500

    Providing Theme Context/Update theme provider. (#21)
    * provide theme-context from terra-application
    
    * consume released theme context
    
    * adding examples and themes
    
    * change log
    
    * Update description
    
    * Update changelog

--- dev-site-config/site.config.js
+++ dev-site-config/site.config.js
@@ -1,5 +1,13 @@
 const siteConfig = {
   includeTestEvidence: false,
+  appConfig: {
+
+    themes: {
+      'Default Theme': '',
+      'Clinical-lowlight-theme': 'clinical-lowlight-theme',
+      'Orion Fusion Theme': 'orion-fusion-theme',
+    },
+  },
 };
 
 module.exports = siteConfig;

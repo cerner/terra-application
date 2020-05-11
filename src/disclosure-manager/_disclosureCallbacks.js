af5f40475a7c554a667353d1765d7b8c0956dba4
David Schoonover <david.schoonover@cerner.com>
2020-05-11 11:07:20 -0500

    Documentation and new screens

--- src/disclosure-manager/_disclosureCallbacks.js
+++ src/disclosure-manager/_disclosureCallbacks.js
@@ -8,6 +8,14 @@ const removeCallback = (callback) => {
   disclosureCallbacks = disclosureCallbacks.filter(disclosureCallback => disclosureCallback !== callback);
 };
 
+/**
+ * Closes the most recent disclosure for any instance of a terra-application
+ * disclosure manager.  This should be used only as a last resort to hook up external events to the disclosure manager
+ * functionality.
+ * @returns {Promise} A promise that will be resolved once the disclosure is successfully dismissed.
+ * If the disclosure is not dismissed the promise will be rejected.  If there is no disclosure available to close,
+ * the promise will be resolved.
+ */
 const closeMostRecentDisclosure = () => {
   if (!disclosureCallbacks.length) {
     return Promise.resolve();
@@ -16,6 +24,11 @@ const closeMostRecentDisclosure = () => {
   return disclosureCallbacks[disclosureCallbacks.length - 1]();
 };
 
+/**
+ * Returns the number of disclosures that are currently being disclosed through any instance of a terra-application
+ * disclosure manager.
+ * @returns {number} The active disclosure count.
+ */
 const getActiveDisclosureCount = () => (disclosureCallbacks.length);
 
 export {

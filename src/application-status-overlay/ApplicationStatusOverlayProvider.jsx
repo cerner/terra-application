e9d227d97d2d46c6351603502606be4104b2cd57
Avinash Gupta <Avinash.Gupta@cerner.com>
2020-07-03 12:46:31 +0530

    made background elements inert

--- src/application-status-overlay/ApplicationStatusOverlayProvider.jsx
+++ src/application-status-overlay/ApplicationStatusOverlayProvider.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import Scroll from 'terra-scroll';
@@ -25,6 +25,8 @@ const propTypes = {
 const ApplicationStatusOverlayProvider = ({ children, scrollRefCallback, ...customProps }) => {
   const [registeredStatusOverlay, setRegisteredStatusOverlay] = React.useState({});
 
+  const refContainer = React.useRef();
+
   const contextValue = useMemo(() => ({
     show: (key, data) => {
       setRegisteredStatusOverlay(state => (
@@ -44,6 +46,19 @@ const ApplicationStatusOverlayProvider = ({ children, scrollRefCallback, ...cust
     },
   }), []);
 
+  const disableContainerChildrenFocus = () => {
+    if (refContainer.current && refContainer.current.querySelector('[data-status-overlay-container-content]')) {
+      refContainer.current.querySelector('[data-status-overlay-container-content]').setAttribute('inert', '');
+    }
+  };
+
+  const enableContainerChildrenFocus = () => {
+    if (refContainer.current && refContainer.current.querySelector('[data-status-overlay-container-content]')) {
+      refContainer.current.querySelector('[data-status-overlay-container-content]').removeAttribute('inert');
+      refContainer.current.querySelector('[data-status-overlay-container-content]').removeAttribute('aria-hidden');
+    }
+  };
+
   const registeredStatusOverlayKeys = Object.keys(registeredStatusOverlay);
   // In the event when multiple ApplicationStatusOverlay's are provided, the last rendered wins
   const lastRegisteredStatusOverlayKey = (registeredStatusOverlayKeys.length > 0) && registeredStatusOverlayKeys[registeredStatusOverlayKeys.length - 1];
@@ -52,6 +67,23 @@ const ApplicationStatusOverlayProvider = ({ children, scrollRefCallback, ...cust
   const registeredMessage = registeredStatusOverlay[lastRegisteredStatusOverlayKey] && registeredStatusOverlay[lastRegisteredStatusOverlayKey].message;
   const registeredTitle = registeredStatusOverlay[lastRegisteredStatusOverlayKey] && registeredStatusOverlay[lastRegisteredStatusOverlayKey].title;
   const registeredVariant = registeredStatusOverlay[lastRegisteredStatusOverlayKey] && registeredStatusOverlay[lastRegisteredStatusOverlayKey].variant;
+
+  useEffect(() => {
+    // eslint-disable-next-line no-prototype-builtins
+    if (!Element.prototype.hasOwnProperty('inert')) {
+      // IE10 throws an error if wicg-inert is imported too early, as wicg-inert tries to set an observer on document.body which may not exist on import
+      // eslint-disable-next-line global-require
+      require('wicg-inert/dist/inert');
+    }
+  });
+
+  useEffect(() => {
+    if (registeredStatusOverlayKeys.length > 0) {
+      disableContainerChildrenFocus();
+    } else {
+      enableContainerChildrenFocus();
+    }
+  }, [registeredStatusOverlayKeys]);
 
   let className = cx('container');
   if (customProps.className) {
@@ -69,14 +101,13 @@ const ApplicationStatusOverlayProvider = ({ children, scrollRefCallback, ...cust
   );
 
   return (
-    <div
-      {...customProps}
-      className={className}
-    >
+    <div {...customProps} ref={refContainer} className={className}>
       {statusView}
       <Scroll refCallback={scrollRefCallback}>
         <ApplicationStatusOverlayContext.Provider value={contextValue}>
-          {children}
+          <div data-status-overlay-container-content className={cx('container-content')}>
+            {children}
+          </div>
         </ApplicationStatusOverlayContext.Provider>
       </Scroll>
     </div>

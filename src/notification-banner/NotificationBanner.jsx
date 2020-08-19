2214fcc0b360837874c9f994e5e09fb7a911602c
Emily Rohrbough <emily.rohrbough@cerner.com>
2020-08-19 11:55:48 -0500

    Reverted "Updated `uuid` dependency to `v8.0.0`" due to IE10 comptability issues

--- src/notification-banner/NotificationBanner.jsx
+++ src/notification-banner/NotificationBanner.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { v4 as uuidv4 } from 'uuid';
+import uuidv4 from 'uuid/v4';
 
 import BannerRegistrationContext from './private/BannerRegistrationContext';
 

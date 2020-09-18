65b1f23c7edda45007a2f39b5c2258ba03b915e6
pranav agarwal <agarwal.pranav300@gmail.com>
2020-09-18 23:37:29 +0530

    Added global styles and inserted empty link to the head

--- src/application-status-overlay/ApplicationStatusOverlayProvider.jsx
+++ src/application-status-overlay/ApplicationStatusOverlayProvider.jsx
@@ -69,6 +69,15 @@ const ApplicationStatusOverlayProvider = ({ children, scrollRefCallback, ...cust
   const registeredVariant = registeredStatusOverlay[lastRegisteredStatusOverlayKey]?.variant;
 
   useEffect(() => {
+    const { head } = document;
+    if (!head.querySelector('link#inert-style')) {
+      if (head.querySelector('style#inert-style')) {
+        head.removeChild(head.querySelector('style#inert-style'));
+      }
+      const link = document.createElement('link');
+      link.id = 'inert-style';
+      head.appendChild(link);
+    }
     // eslint-disable-next-line no-prototype-builtins
     if (!Element.prototype.hasOwnProperty('inert')) {
       // IE10 throws an error if wicg-inert is imported too early, as wicg-inert tries to set an observer on document.body which may not exist on import

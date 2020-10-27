9cf536b2b9ab119354e69b4c70d112b01a1e7a25
Tyler Biethman <tbiethman@gmail.com>
2020-10-27 16:52:09 -0500

    Don't need this guy

--- tests/wdio/util/disableCSSAnimations.js
+++
@@ -1,9 +0,0 @@
-const disableCSSAnimations = (browser) => {
-  browser.execute(`
-    var animationOverride = document.createElement('style');
-    animationOverride.appendChild(document.createTextNode('body * { animation: none !important; }'));
-    document.head.appendChild(animationOverride);
-  `);
-};
-
-export default disableCSSAnimations;

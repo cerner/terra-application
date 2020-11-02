9c93b45e992da158b0d97fca823a86bc91465e82
pranav agarwal <agarwal.pranav300@gmail.com>
2020-11-03 01:49:19 +0530

    added generator and browserslistrc file

---
+++ scripts/browserslistrc-generator/generatorBrowserslistrc.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const path = require('path');
+
+let browserslist = require('@cerner/browserslist-config-terra');
+
+const packagePath = path.join(__dirname, '../../');
+const browsersListFile = path.resolve(packagePath, '.browserslistrc');
+
+if (fs.existsSync(browsersListFile)) {
+  fs.truncate(browsersListFile, 0, () => {});
+}
+
+browserslist = browserslist.toString().split(',').join('\n');
+
+fs.writeFile(browsersListFile, browserslist, () => {});

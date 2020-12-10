f7469c77bb3cea064d745ab15cc1bcb05ea19d1f
Tyler Biethman <tbiethman@gmail.com>
2020-12-10 15:10:42 -0600

    Fixing overlay import

--- src/terra-dev-site/demo-application/workspace/Tab1.jsx
+++ src/terra-dev-site/demo-application/workspace/Tab1.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import WorkspaceContent from '../../../workspace/WorkspaceContent';
-import ApplicationLoadingOverlay from 'terra-application/lib/application-loading-overlay';
+import ApplicationLoadingOverlay from '../../../application-loading-overlay';
 import MockContent from './MockContent';
 
-const Tab1 = () => {
-  return (
-    <WorkspaceContent
-      toolBar={<p style={{ backgroundColor: 'pink', padding: '0.5rem', margin: '0' }}>Tab 1 Toolbar</p>}
-    >
-      <MockContent title="Tab 1" />
-      <ApplicationLoadingOverlay backgroundStyle="dark" />
-    </WorkspaceContent>
-  );
-};
+const Tab1 = () => (
+  <WorkspaceContent
+    toolBar={<p style={{ backgroundColor: 'pink', padding: '0.5rem', margin: '0' }}>Tab 1 Toolbar</p>}
+  >
+    <MockContent title="Tab 1" />
+    <ApplicationLoadingOverlay backgroundStyle="dark" />
+  </WorkspaceContent>
+);
 
 Tab1.titleKey = 'derp';
 

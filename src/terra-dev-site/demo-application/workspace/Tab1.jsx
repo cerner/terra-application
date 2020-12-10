2b6cea86ab13f7c56ae3faed2f971ca444028e47
Kasper,Dave <dave.m.kasper@gmail.com>
2020-12-10 15:06:40 -0600

    Doc and test out loading overlay

--- src/terra-dev-site/demo-application/workspace/Tab1.jsx
+++ src/terra-dev-site/demo-application/workspace/Tab1.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import WorkspaceContent from '../../../workspace/WorkspaceContent';
+import ApplicationLoadingOverlay from 'terra-application/lib/application-loading-overlay';
 import MockContent from './MockContent';
 
 const Tab1 = () => {
@@ -8,6 +9,7 @@ const Tab1 = () => {
       toolBar={<p style={{ backgroundColor: 'pink', padding: '0.5rem', margin: '0' }}>Tab 1 Toolbar</p>}
     >
       <MockContent title="Tab 1" />
+      <ApplicationLoadingOverlay backgroundStyle="dark" />
     </WorkspaceContent>
   );
 };

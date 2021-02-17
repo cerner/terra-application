4ecf9ce1a54d0fe53476c6b4ecb41d065b788ab3
Tyler Biethman <tbiethman@gmail.com>
2021-02-17 09:56:19 -0600

    Removing terra-alert dependency.

--- src/terra-dev-site/test/workspace/MockContent.jsx
+++ src/terra-dev-site/test/workspace/MockContent.jsx
@@ -25,50 +25,6 @@ const MockContent = ({
   const [showWarningBanner, setShowWarningBanner] = useState(false);
   const [showErrorBanner, setShowErrorBanner] = useState(false);
 
-  // React.useEffect(() => {
-  //   setTimeout(() => {
-  //     setShowAlertBanner(true);
-  //   }, 5000);
-  //   setTimeout(() => {
-  //     setShowWarningBanner(true);
-  //   }, 8000);
-  //   setTimeout(() => {
-  //     setShowErrorBanner(true);
-  //   }, 11000);
-
-  //   setTimeout(() => {
-  //     setShowAlertBanner(false);
-  //   }, 12000);
-  //   setTimeout(() => {
-  //     setShowWarningBanner(false);
-  //   }, 13000);
-  //   setTimeout(() => {
-  //     setShowErrorBanner(false);
-  //   }, 14000);
-  // }, []);
-
-  // React.useEffect(() => {
-  //   setTimeout(() => {
-  //     setShowAlertBanner(true);
-  //   }, 5000);
-  //   setTimeout(() => {
-  //     setShowWarningBanner(true);
-  //   }, 5000);
-  //   setTimeout(() => {
-  //     setShowErrorBanner(true);
-  //   }, 5000);
-
-  //   setTimeout(() => {
-  //     setShowAlertBanner(false);
-  //   }, 10000);
-  //   setTimeout(() => {
-  //     setShowWarningBanner(false);
-  //   }, 10000);
-  //   setTimeout(() => {
-  //     setShowErrorBanner(false);
-  //   }, 10000);
-  // }, []);
-
   return (
     <div className={cx('content-container')}>
       <button type="button" onClick={() => setClickCount(clickCount + 1)}>Increment Counter</button>

78d92d0d96ce95d1ade2b4f5860bdf1031bafbd1
Matt Henkes <mjhenkes@gmail.com>
2021-03-26 10:33:25 -0500

    [terra-application-v2] Re-enable wdio testing in terra application v2 (#171)
    * Re-enable wdio testing in terra application v2
    
    * Update travis
    
    * Switch over to hash routing.

--- wdio.conf.js
+++ wdio.conf.js
@@ -9,4 +9,45 @@ if (travis) {
   wdioConfig.host = 'localhost';
 }
 
+const defaultBefore = wdioConfig.before;
+wdioConfig.before = () => {
+  if (defaultBefore) {
+    defaultBefore();
+  }
+
+  global.browser.addCommand('disableCSSAnimations', () => {
+    global.browser.execute(`
+      if (!document.getElementById('wdio-animation-override')) {
+        var animationOverride = document.createElement('style');
+        animationOverride.id = 'wdio-animation-override';
+        animationOverride.appendChild(document.createTextNode('body * { animation: none !important; }'));
+        document.head.appendChild(animationOverride);
+      }
+    `);
+  });
+
+  global.browser.addCommand('enableCSSAnimations', () => {
+    global.browser.execute(`
+      var animationOverride = document.getElementById('wdio-animation-override');
+      if (animationOverride) {
+        animationOverride.parentNode.removeChild(animationOverride);
+      }
+    `);
+  });
+
+  global.browser.addCommand('clickWithTestId', (testId) => {
+    const selector = `[data-testid="${testId}"]`;
+
+    global.browser.waitForVisible(selector);
+    global.browser.click(selector);
+  });
+
+  global.browser.addCommand('clickWithAttribute', (key, value) => {
+    const selector = `[${key}=${value}]`;
+
+    global.browser.waitForVisible(selector);
+    global.browser.click(selector);
+  });
+};
+
 exports.config = wdioConfig;

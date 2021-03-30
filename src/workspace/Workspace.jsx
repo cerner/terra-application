771ddb8dfea425d532fb9c820d35b4a4349fa73d
Tyler Biethman <tbiethman@users.noreply.github.com>
2021-03-30 15:53:14 -0500

    Updating Workspace styling to match new designs (#169)
    * First pass open source theme. Need to update themeable variables for other themes and delete unused values.
    
    * First pass of colors and positions for lowlight and fusion theme.
    
    * Working RTL flip.
    
    * Adding new workspace container styles
    
    * Missed a couple
    
    * Adding test for new prop. Adding new testing endpoint for wdio tests of overlay styles.
    
    * Cleaning up lint. Updating Changelog.
    
    * Updated tab hover.
    
    * Fix for offset with fusion theme. Restore margins.
    
    * Updating wdio screenshots
    
    * Fixing screenshots?
    
    * One more time with the screenshots
    
    * Remove todos
    
    * Tweak
    
    * Updated margins for tab offsets.
    
    * Rem to px for box shadow padding
    
    * Whoops
    
    * Double whoops
    
    * Correct dropdown base positioning.
    
    * Correct for border. Needs vertical fix.
    
    * Fix safari rounding. I need some advil.
    
    * Update lint and screenshots.
    
    * Updated compiled screens.
    
    * Removing themable variable for visibility in workspace components.
    
    * Clean up variables.
    
    * Alter menu tab is-open click functionality.
    
    * Color and fix escape.
    
    * Fixed mpage theme border clipping
    
    * Update rounded sub pixel.
    
    * Provide separate variables for icon and label.
    
    * Fix capitalization.
    
    * Update screenshots for new colors.
    
    * Revise color implementation for tabs.
    
    Co-authored-by: Kasper,Dave <dave.m.kasper@gmail.com>
    Co-authored-by: Matt Henkes <mjhenkes@gmail.com>

--- src/workspace/Workspace.jsx
+++ src/workspace/Workspace.jsx
@@ -61,6 +61,11 @@ const propTypes = {
    */
   dismissButtonIsVisible: PropTypes.bool,
   /**
+   * Whether or not the Workspace is being presented as an overlay and thus
+   * should render with its overlay-specific styling.
+   */
+  isPresentedAsOverlay: PropTypes.bool,
+  /**
    * The function callback triggering when a item is selected.
    * Returns the associated itemKey and metaData. e.g. onRequestActivate(itemKey, metaData)
    */
@@ -104,6 +109,7 @@ const Workspace = ({
   activeSize,
   children,
   dismissButtonIsVisible,
+  isPresentedAsOverlay,
   onRequestActivate,
   onRequestSizeChange,
   onRequestDismiss,
@@ -212,9 +218,10 @@ const Workspace = ({
     );
   }
 
-  const workspaceClassNames = classNames(
+  const containerClassNames = classNames(
     cx(
-      'workspace',
+      'workspace-container',
+      { 'is-overlay': isPresentedAsOverlay },
       theme.className,
     ),
     customProps.className,
@@ -224,39 +231,47 @@ const Workspace = ({
     <div
       {...customProps}
       id={id}
-      className={workspaceClassNames}
+      className={containerClassNames}
       role="none"
     >
-      <div role="none" className={cx('button-header')}>
-        {dismissButton}
-        <div className={cx('fill-element')} />
-        {sizeButton}
-      </div>
-      <div role="none" className={cx('tab-header', { 'has-dismiss-button': onRequestDismiss && dismissButtonIsVisible })}>
-        <Tabs ariaLabel={ariaLabel} tabData={tabData} />
-      </div>
-      <div role="none" className={cx('body')} ref={workspaceContainerRef}>
-        {React.Children.map(children, child => {
-          let portalElement = workspacePortalsRef.current[child.props.itemKey]?.element;
-          if (!portalElement) {
-            portalElement = getPortalElement();
-            portalElement.setAttribute('role', 'none');
+      <div
+        className={cx('workspace')}
+        role="none"
+      >
+        <div aria-hidden className={cx('body-shadow-container')}>
+          <div className={cx('body-shadow')} />
+        </div>
+        <div role="none" className={cx('button-header')}>
+          {dismissButton}
+          <div className={cx('fill-element')} />
+          {sizeButton}
+        </div>
+        <div role="none" className={cx('tab-header', { 'has-dismiss-button': onRequestDismiss && dismissButtonIsVisible })}>
+          <Tabs ariaLabel={ariaLabel} tabData={tabData} />
+        </div>
+        <div role="none" className={cx('body')} ref={workspaceContainerRef}>
+          {React.Children.map(children, child => {
+            let portalElement = workspacePortalsRef.current[child.props.itemKey]?.element;
+            if (!portalElement) {
+              portalElement = getPortalElement();
+              portalElement.setAttribute('role', 'none');
 
-            workspacePortalsRef.current[child.props.itemKey] = {
-              element: portalElement,
-            };
-          }
+              workspacePortalsRef.current[child.props.itemKey] = {
+                element: portalElement,
+              };
+            }
 
-          return (
-            React.cloneElement(child, {
-              key: child.props.itemKey,
-              id: getTabId(id, child.props.itemKey),
-              associatedPanelId: getAssociatedPanelId(id, child.props.itemKey),
-              isActive: child.props.itemKey === activeItemKey,
-              portalElement,
-            })
-          );
-        })}
+            return (
+              React.cloneElement(child, {
+                key: child.props.itemKey,
+                id: getTabId(id, child.props.itemKey),
+                associatedPanelId: getAssociatedPanelId(id, child.props.itemKey),
+                isActive: child.props.itemKey === activeItemKey,
+                portalElement,
+              })
+            );
+          })}
+        </div>
       </div>
     </div>
   );

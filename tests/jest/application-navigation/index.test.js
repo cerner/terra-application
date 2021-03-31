957ad41c22fd6d9575977a813901c57629b2c7dc
Tyler Biethman <tbiethman@gmail.com>
2021-03-31 13:20:27 -0500

    Removing legacy application-navigation export

--- tests/jest/application-navigation/index.test.js
+++
@@ -1,7 +0,0 @@
-import ApplicationNavigation from '../../../src/application-navigation';
-
-describe('application-navigation/index', () => {
-  it('should export ApplicationNavigation', () => {
-    expect(ApplicationNavigation).toBeDefined();
-  });
-});

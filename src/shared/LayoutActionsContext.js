445e35b3949f396f8f4d57a83bbb79a1ff7ea701
Tyler Biethman <tbiethman@gmail.com>
2021-05-05 10:05:59 -0500

    Missed one

--- src/shared/LayoutActionsContext.js
+++ src/shared/LayoutActionsContext.js
@@ -1,17 +1,46 @@
 import { createContext } from 'react';
 import PropTypes from 'prop-types';
 
+/**
+ * The LayoutActionsContext can be used by Layout implementations to define
+ * actions to be rendered by content rendered within the Layout. It is up
+ * to the context's consumer to determine how and where these action controls
+ * are rendered.
+ */
 const LayoutActionsContext = createContext();
 
 const actionShape = PropTypes.shape({
+  /**
+   * An identifier for the action. This must be unique within the provided
+   * actions.
+   */
   key: PropTypes.string.isRequired,
+  /**
+   * A string description for the action. This will accessible to users and
+   * should be translated sif necessary.
+   */
   label: PropTypes.string.isRequired,
+  /**
+   * A graphic representing the action. This may be used by the context consumer
+   * to render controls for the action.
+   */
   icon: PropTypes.elementType,
+  /**
+   * A function executed upon selection of the action.
+   */
   onSelect: PropTypes.func,
 });
 
 const contextShape = {
+  /**
+   * Action definitions related to actions that are relevant to the Layout's
+   * leading (as determined by LTR/RTL) side.
+   */
   startActions: PropTypes.arrayOf(actionShape),
+  /**
+   * Action definitions related to actions that are relevant to the Layout's
+   * trailing (as determined by LTR/RTL) side.
+   */
   endActions: PropTypes.arrayOf(actionShape),
 };
 

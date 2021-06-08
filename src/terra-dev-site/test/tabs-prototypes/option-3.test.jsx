94ad99c221e80a97bc5159d9af2a9bd05399d10f
Emilio Sanchez <Emilio_Sanchez@epam.com>
2021-06-08 12:14:51 -0500

    double quotes fix

--- src/terra-dev-site/test/tabs-prototypes/option-3.test.jsx
+++ src/terra-dev-site/test/tabs-prototypes/option-3.test.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import Workspace, { WorkspaceItem } from '../../../workspace';
-import ActiveMainPageContext from '../../../application-container/private/active-main-page/ActiveMainPageContext';
-import Tab1 from './Tab1';
-import Tab2 from './Tab2';
-import Tab3 from './Tab3';
-import Tab4 from './Tab4';
-import Tab5 from './Tab5';
-import Tab6 from './Tab6';
+import React from "react";
+import Workspace, { WorkspaceItem } from "../../../workspace";
+import ActiveMainPageContext from "../../../application-container/private/active-main-page/ActiveMainPageContext";
+import Tab1 from "./Tab1";
+import Tab2 from "./Tab2";
+import Tab3 from "./Tab3";
+import Tab4 from "./Tab4";
+import Tab5 from "./Tab5";
+import Tab6 from "./Tab6";
 
 const sizeMap = {
-  small: '320px',
-  medium: '500px',
-  large: '1000px',
+  small: "320px",
+  medium: "500px",
+  large: "1000px",
 };
 
 const WorkspaceTest = () => {
-  const [activeItemKey, setActiveItemKey] = React.useState('tab-1');
-  const [workspaceSize, setWorkspaceSize] = React.useState('large');
+  const [activeItemKey, setActiveItemKey] = React.useState("tab-1");
+  const [workspaceSize, setWorkspaceSize] = React.useState("large");
   const activeMainPageRef = React.useRef({
-    pageKey: 'page-1',
-    pageLabel: 'Test Page',
+    pageKey: "page-1",
+    pageLabel: "Test Page",
     pageMetaData: {
-      data: 'data here',
+      data: "data here",
     },
   });
 
@@ -30,13 +30,13 @@ const WorkspaceTest = () => {
   };
 
   const onRequestClose = () => {
-    console.log('onRequestClose'); // eslint-disable-line no-console
+    console.log("onRequestClose"); // eslint-disable-line no-console
   };
 
   return (
     <ActiveMainPageContext.Provider value={activeMainPageRef.current}>
       <div
-        style={{ height: '100%', width: sizeMap[workspaceSize] }} // eslint-disable-line react/forbid-dom-props
+        style={{ height: "100%", width: sizeMap[workspaceSize] }} // eslint-disable-line react/forbid-dom-props
       >
         <Workspace
           id="overlay-test-id"
@@ -47,16 +47,16 @@ const WorkspaceTest = () => {
           activeSize={workspaceSize}
           sizeOptions={[
             {
-              key: 'small',
-              text: 'Small',
+              key: "small",
+              text: "Small",
             },
             {
-              key: 'medium',
-              text: 'Medium',
+              key: "medium",
+              text: "Medium",
             },
             {
-              key: 'large',
-              text: 'Large',
+              key: "large",
+              text: "Large",
             },
           ]}
           onRequestSizeChange={onRequestSizeChange}

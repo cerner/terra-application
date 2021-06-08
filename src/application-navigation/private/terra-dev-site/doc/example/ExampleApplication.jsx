84bebd3e87ed240e8c1a99792bab9daf9bf33ce3
Kasper,Dave <dave.m.kasper@gmail.com>
2021-06-08 12:04:38 -0500

    Basic file structure, needs theme files.

---
+++ src/application-navigation/private/terra-dev-site/doc/example/ExampleApplication.jsx
@@ -0,0 +1,166 @@
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
+import IconSearch from 'terra-icon/lib/icon/IconSearch';
+import IconPill from 'terra-icon/lib/icon/IconPill';
+import IconVisualization from 'terra-icon/lib/icon/IconVisualization';
+import IconLightbulb from 'terra-icon/lib/icon/IconLightbulb';
+import { Placeholder } from '@cerner/terra-docs';
+import ExampleWrapper from './ExampleWrapper';
+import ApplicationNavigation from '../../../ApplicationNavigation';
+
+const titleConfig = {
+  title: 'Test Title',
+};
+
+const userConfig = {
+  name: 'Test Name',
+  initials: 'TN',
+};
+
+const extensionItems = [
+  {
+    icon: <IconSearch />,
+    key: 'item-a',
+    text: 'Item A',
+    metaData: {
+      test: 'a',
+    },
+  }, {
+    icon: <IconPill />,
+    key: 'item-b',
+    text: 'Item B',
+    metaData: {
+      test: 'b',
+    },
+  }, {
+    icon: <IconVisualization />,
+    key: 'item-c',
+    text: 'Item C',
+    metaData: {
+      test: 'c',
+    },
+  }, {
+    icon: <IconLightbulb />,
+    key: 'item-d',
+    text: 'Item D',
+    metaData: {
+      test: 'd',
+    },
+  },
+];
+
+const navigationItems = [
+  {
+    key: '/page_1',
+    text: 'Page 1',
+    metaData: {
+      display: 'Page 1',
+    },
+  }, {
+    key: '/page_2',
+    text: 'Page 2',
+    metaData: {
+      display: 'Page 2',
+    },
+  }, {
+    key: '/page_3',
+    text: 'Page 3',
+    metaData: {
+      display: 'Page 3',
+    },
+  }, {
+    key: '/page_4',
+    text: 'Page 4',
+    metaData: {
+      display: 'Page 4',
+    },
+  }, {
+    key: '/page_5',
+    text: 'Page 5',
+    metaData: {
+      display: 'Page 5',
+    },
+  }, {
+    key: '/page_6',
+    text: 'Page 6',
+    metaData: {
+      display: 'Page 6',
+    },
+  }, {
+    key: '/page_7',
+    text: 'Page 7',
+    metaData: {
+      display: 'Page 7',
+    },
+  }, {
+    key: '/page_8',
+    text: 'Page 8',
+    metaData: {
+      display: 'Page 8',
+    },
+  },
+];
+
+const utilityItems = [
+  {
+    icon: <IconSearch />,
+    key: 'item-a',
+    text: 'Item A',
+    metaData: {
+      test: 'a',
+    },
+  }, {
+    icon: <IconPill />,
+    key: 'item-b',
+    text: 'Item B',
+    metaData: {
+      test: 'b',
+    },
+  }, {
+    icon: <IconVisualization />,
+    key: 'item-c',
+    text: 'Item C',
+    metaData: {
+      test: 'c',
+    },
+  }, {
+    icon: <IconLightbulb />,
+    key: 'item-d',
+    text: 'Item D',
+    metaData: {
+      test: 'd',
+    },
+  },
+];
+
+const ExampleApplication = ({ onAction }) => {
+  const [activeKey, setActiveKey] = useState(navigationItems[0].key);
+
+  return (
+    <ApplicationNavigation
+      titleConfig={titleConfig}
+      userConfig={userConfig}
+      extensionItems={extensionItems}
+      onSelectExtensionItem={onAction}
+      navigationItems={navigationItems}
+      activeNavigationItemKey={activeKey}
+      onSelectNavigationItem={key => setActiveKey(key)}
+      utilityItems={utilityItems}
+      onSelectUtilityItem={onAction}
+      onSelectSettings={() => onAction('settings')}
+      onSelectHelp={() => onAction('help')}
+      onSelectLogout={() => onAction('logout')}
+      id="example-application"
+    >
+      <Placeholder title={activeKey} />
+    </ApplicationNavigation>
+  );
+};
+
+const ExampleNavigation = () => (
+  <ExampleWrapper>
+    <ExampleApplication />
+  </ExampleWrapper>
+);
+
+export default ExampleNavigation;
---
+++ src/application-navigation/private/terra-dev-site/test/application-navigation/Title.test.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import ApplicationNavigation from '../../../ApplicationNavigation';
+import FixedHeight from '../common/FixedHeight';
+import Logo from '../common/Logo';
+
+const titleConfig = {
+  title: 'Test Title',
+};
+
+const titleConfigHeadLine = {
+  title: 'Test Title',
+  headline: 'Test HeadLine',
+};
+
+const titleConfigSubline = {
+  title: 'Test Title',
+  subline: 'Test Subline',
+};
+
+const titleConfigElement = {
+  title: 'Test Title',
+  element: <Logo />,
+};
+
+const titleConfigHideTitle = {
+  title: 'Test Title',
+  hideTitleWithinDrawerMenu: true,
+};
+
+// TODO: remove terra-application after it is incorporated into dev-site for themes or themes are co-located
+const TitleTest = () => (
+  <>
+    <FixedHeight><ApplicationNavigation titleConfig={titleConfig} /></FixedHeight>
+    <FixedHeight><ApplicationNavigation titleConfig={titleConfigHeadLine} /></FixedHeight>
+    <FixedHeight><ApplicationNavigation titleConfig={titleConfigSubline} /></FixedHeight>
+    <FixedHeight><ApplicationNavigation titleConfig={titleConfigElement} /></FixedHeight>
+    <FixedHeight><ApplicationNavigation titleConfig={titleConfigHideTitle} /></FixedHeight>
+  </>
+);
+
+export default TitleTest;

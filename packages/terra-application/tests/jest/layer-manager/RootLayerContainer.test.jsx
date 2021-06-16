dbd513c06315d5c704e87ee669ec49be4cb32fd7
Tyler Biethman <tbiethman@gmail.com>
2021-06-16 10:22:50 -0500

    Fixing lint

--- packages/terra-application/tests/jest/layer-manager/RootLayerContainer.test.jsx
+++ packages/terra-application/tests/jest/layer-manager/RootLayerContainer.test.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import RootLayerContainer from '../../../src/layer-manager/RootLayerContainer';
-import Logger from '../../../src/utils/logger';
 
 import MockApplication from '../MockApplication';
 import MockLayer from '../MockLayer';

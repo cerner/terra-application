87c1eddf46786f1b12746217bb55218e20f9c16a
Kasper,Dave <dave.m.kasper@gmail.com>
2021-06-21 14:50:36 -0500

    Initial files, next will start pulling apart.

---
+++ src/application-navigation/workspace-layout/PrimaryNavigationWorkspaceItem.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/* eslint-disable no-unused-vars, react/no-unused-prop-types */
+const propTypes = {
+  /**
+   * Key to match with the activeItemKey to handle the display of selection.
+   */
+  itemKey: PropTypes.string.isRequired,
+  /**
+   * Text to be displayed on the item or as it's aria-label.
+   */
+  label: PropTypes.string.isRequired,
+  /**
+   * Object to be returned in the onRequestActive.
+   */
+  metaData: PropTypes.object,
+  /**
+   * The render block for the workspace item
+   */
+  render: PropTypes.func,
+};
+
+const PrimaryNavigationWorkspaceItem = () => <React.Fragment />;
+
+PrimaryNavigationWorkspaceItem.propTypes = propTypes;
+
+export default PrimaryNavigationWorkspaceItem;

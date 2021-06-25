56804d45aa9f9813d602e6322cae6856e96ea13f
Tyler Biethman <tbiethman@gmail.com>
2021-06-25 11:28:35 -0500

    Adding sanity check wdio tests

---
+++ packages/terra-application/tests/wdio/primary-navigation-layout/primary-navigation-layout-3-spec.js
@@ -0,0 +1,22 @@
+const selector = '#root';
+
+Terra.describeViewports('PrimaryNavigationLayout - Configuration 3', ['small', 'enormous'], () => {
+  before(() => {
+    browser.url('/terra-application-test/#/primary-navigation-layout-3');
+  });
+
+  after(() => {
+    browser.refresh();
+  });
+
+  it('renders as expected for size', () => {
+    Terra.validates.element('1. initial rendering with page', { selector });
+  });
+
+  it('renders with nested Page', () => {
+    browser.clickWithTestId('test-page-show-child-page');
+    Terra.validates.element('2. render with nested page', { selector });
+    browser.refresh();
+  });
+});
+

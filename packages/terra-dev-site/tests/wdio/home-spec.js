0fc50342a29eb3b5ab64ab56db719c90cfcef304
Matt Henkes <mjhenkes@gmail.com>
2021-07-20 15:42:06 -0500

    add test screenshots

--- packages/terra-dev-site/tests/wdio/home-spec.js
+++ packages/terra-dev-site/tests/wdio/home-spec.js
@@ -1,12 +1,6 @@
 Terra.describeViewports('home', ['tiny', 'huge'], () => {
   it('checks accessibility', () => {
     browser.url('/');
-    $('a [alt="devDependencies status"]').waitForDisplayed();
-    $('a [alt="Dependencies status"]').waitForDisplayed();
-    $('a [alt="Build Status"]').waitForDisplayed();
-    $('a [alt="License"]').waitForDisplayed();
-    $('a [alt="Cerner OSS"]').waitForDisplayed();
-    $('a [alt="NPM version"]').waitForDisplayed();
     Terra.validates.accessibility({ selector: '#root' });
   });
 });

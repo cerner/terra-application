27c54f1a6b425147b56e17db1fde6eb09410f018
Supreeth MR <supreeth.mr1990@gmail.com>
2022-06-04 00:03:06 +0530

    removed index import

--- packages/terra-polyfill/src/index.js
+++ packages/terra-polyfill/src/index.js
@@ -47,11 +47,3 @@ import './polyfills/inert-polyfill';
  * Polyfill for [Intl](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl).
  */
 import './polyfills/intl-polyfill';
-
-/**
- * Polyfill for [Intl.DateTimeFormat() Options parameter](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat).
- * `options` parameter of Date and Intl functions are not supported in IE10 hence adding this polyfill to provide support for `options` parameter in IE10. : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat#browser_compatibility
- */
-import intlDateTimeFormatPolyfill from './polyfills/intlDateTimeFormatPolyfill';
-
-export { intlDateTimeFormatPolyfill };
--- packages/terra-polyfill/src/polyfills/intlDateTimeFormatPolyfill.js
+++ packages/terra-polyfill/src/polyfills/intlDateTimeFormatPolyfill.js
@@ -1,4 +1,8 @@
 /* eslint-disable global-require, import/no-dynamic-require */
+/**
+ * Polyfill for [Intl.DateTimeFormat() Options parameter](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat).
+ * `options` parameter of Date and Intl functions are not supported in IE10 hence adding this polyfill to provide support for `options` parameter in IE10. : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat#browser_compatibility
+ */
 import '@formatjs/intl-getcanonicallocales/polyfill';
 import '@formatjs/intl-locale/polyfill';
 

cfb8bb57213c3e15b2e1730b1a5635cf2d750369
Supreeth MR <supreeth.mr1990@gmail.com>
2022-06-03 19:06:25 +0530

    comment index changes to prevent accesiblity error

--- packages/terra-polyfill/src/index.js
+++ packages/terra-polyfill/src/index.js
@@ -48,10 +48,10 @@ import './polyfills/inert-polyfill';
  */
 import './polyfills/intl-polyfill';
 
-/**
- * Polyfill for [Intl.DateTimeFormat() Options parameter](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat).
- * `options` parameter of Date and Intl functions are not supported in IE10 hence adding this polyfill to provide support for `options` parameter in IE10. : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat#browser_compatibility
- */
-import intlDateTimeFormatPolyfill from './polyfills/intlDateTimeFormatPolyfill';
+// /**
+//  * Polyfill for [Intl.DateTimeFormat() Options parameter](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat).
+//  * `options` parameter of Date and Intl functions are not supported in IE10 hence adding this polyfill to provide support for `options` parameter in IE10. : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat#browser_compatibility
+//  */
+// import intlDateTimeFormatPolyfill from './polyfills/intlDateTimeFormatPolyfill';
 
-export { intlDateTimeFormatPolyfill };
+// export { intlDateTimeFormatPolyfill };

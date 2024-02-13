550a47a3129090707ffe83abf5be2f565eb94ed9
BenBoersma <Ben.Boersma@cerner.com>
2024-02-13 15:42:37 -0600

    trying dynamic import

--- packages/terra-application/src/application-base/private/translationsLoaders.js
+++ packages/terra-application/src/application-base/private/translationsLoaders.js
@@ -4,37 +4,38 @@ import logger from '../../utils/logger';
 const loadTranslationsFile = (locale) => {
   switch (locale) {
     case 'en':
-      return import(/* webpackChunkName: "en-translations" */ 'en.js');
+//      return import(/* webpackChunkName: "en-translations" */ 'en.js');
     case 'en-AU':
-      return import(/* webpackChunkName: "en-AU-translations" */ 'en-AU.js');
+//      return import(/* webpackChunkName: "en-AU-translations" */ 'en-AU.js');
     case 'en-CA':
-      return import(/* webpackChunkName: "en-CA-translations" */ 'en-CA.js');
+//      return import(/* webpackChunkName: "en-CA-translations" */ 'en-CA.js');
     case 'en-US':
-      return import(/* webpackChunkName: "en-US-translations" */ 'en-US.js');
+//      return import(/* webpackChunkName: "en-US-translations" */ 'en-US.js');
     case 'en-GB':
-      return import(/* webpackChunkName: "en-GB-translations" */ 'en-GB.js');
+//      return import(/* webpackChunkName: "en-GB-translations" */ 'en-GB.js');
     case 'es':
-      return import(/* webpackChunkName: "es-translations" */ 'es.js');
+//      return import(/* webpackChunkName: "es-translations" */ 'es.js');
     case 'es-ES':
-      return import(/* webpackChunkName: "es-ES-translations" */ 'es-ES.js');
+//      return import(/* webpackChunkName: "es-ES-translations" */ 'es-ES.js');
     case 'de':
-      return import(/* webpackChunkName: "de-translations" */ 'de.js');
+//      return import(/* webpackChunkName: "de-translations" */ 'de.js');
     case 'fr':
-      return import(/* webpackChunkName: "fr-translations" */ 'fr.js');
+//      return import(/* webpackChunkName: "fr-translations" */ 'fr.js');
     case 'fr-FR':
-      return import(/* webpackChunkName: "fr-FR-translations" */ 'fr-FR.js');
+//      return import(/* webpackChunkName: "fr-FR-translations" */ 'fr-FR.js');
     case 'nl':
-      return import(/* webpackChunkName: "nl-translations" */ 'nl.js');
+//      return import(/* webpackChunkName: "nl-translations" */ 'nl.js');
     case 'nl-BE':
-      return import(/* webpackChunkName: "nl-BE-translations" */ 'nl-BE.js');
+//      return import(/* webpackChunkName: "nl-BE-translations" */ 'nl-BE.js');
     case 'pt':
-      return import(/* webpackChunkName: "pt-translations" */ 'pt.js');
+//      return import(/* webpackChunkName: "pt-translations" */ 'pt.js');
     case 'pt-BR':
-      return import(/* webpackChunkName: "pt-BR-translations" */ 'pt-BR.js');
+//      return import(/* webpackChunkName: "pt-BR-translations" */ 'pt-BR.js');
     case 'sv':
-      return import(/* webpackChunkName: "sv-translations" */ 'sv.js');
+//      return import(/* webpackChunkName: "sv-translations" */ 'sv.js');
     case 'sv-SE':
-      return import(/* webpackChunkName: "sv-SE-translations" */ 'sv-SE.js');
+//      return import(/* webpackChunkName: "sv-SE-translations" */ 'sv-SE.js');
+      return import(`${locale}.js`);
     default:
       return Promise.reject(Error(`Translations were not supplied for the ${locale} locale.`));
   }

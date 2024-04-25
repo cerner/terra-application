e2e39ce7fc3513deed54d004c63d9729dc48620a
Saad <38024451+sdadn@users.noreply.github.com>
2024-04-25 12:44:47 -0500

    fix enzyme import

--- jest.enzymeSetup.js
+++ jest.enzymeSetup.js
@@ -2,7 +2,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable object-curly-newline */
 import Enzyme, { mount, render, shallow } from 'enzyme';
-import { mockIntl, mountWithIntl, renderWithIntl, shallowWithIntl } from 'terra-enzyme-intl';
+import { mockIntl, mountWithIntl, renderWithIntl, shallowWithIntl } from '@cerner/terra-enzyme-intl';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });

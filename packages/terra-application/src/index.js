4696c2a0f40f2709986a7f17df2216f16a08f326
Saad <38024451+sdadn@users.noreply.github.com>
2024-04-25 15:28:34 -0500

    added named exports

---
+++ packages/terra-application/src/index.js
@@ -0,0 +1,18 @@
+import ApplicationBase from './application-base';
+
+export default ApplicationBase;
+export * from 'application-base';
+export * from 'application-client';
+export * from 'application-error-boundary';
+export * from 'application-intl';
+export * from 'application-loading-overlay';
+export * from 'application-navigation';
+export * from 'breakpoints';
+export * from 'dislosure-manager';
+export * from 'modal-manager';
+export * from 'navigation-prompt';
+export * from 'notification-banner';
+export * from 'orion-requestor';
+export * from 'side-panel-manager';
+export * from 'theme';
+export * from 'workspace';

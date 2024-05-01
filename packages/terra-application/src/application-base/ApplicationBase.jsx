a356580ce1d3595e4684cbdd66781dce445ba359
Saad Adnan <38024451+sdadn@users.noreply.github.com>
2024-05-01 12:13:54 -0500

    Update ApplicationBase.jsx

--- packages/terra-application/src/application-base/ApplicationBase.jsx
+++ packages/terra-application/src/application-base/ApplicationBase.jsx
@@ -1,6 +1,6 @@
 /* global TERRA_THEME_CONFIG */
 
-import React, { useRef, useEffect, Suspense, useMemo, useState } from 'react';
+import React, { useRef, useEffect, useMemo, useState, Suspense } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import ThemeProvider from 'terra-theme-provider';

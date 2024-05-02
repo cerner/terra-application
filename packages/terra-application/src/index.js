6136f9dfb9acf2c1302b30b7fc036a7cb329fd80
Saad <38024451+sdadn@users.noreply.github.com>
2024-05-02 15:52:10 -0500

    updated imports

--- packages/terra-application/src/index.js
+++ packages/terra-application/src/index.js
@@ -1,4 +1,4 @@
-import ApplicationBase from './application-base';
+import TerraApplicationBase from './application-base';
 
 import * as ApplicationNavigation from './application-navigation';
 
@@ -7,19 +7,17 @@ export {
 }
 
 
-export default ApplicationBase;
-export * from './application-base';
-export * from './application-client';
-export * from './application-error-boundary';
-export * from './application-intl';
-export * from './application-loading-overlay';
-export * from './application-navigation';
-export * from './breakpoints';
-export * from './dislosure-manager';
-export * from './modal-manager';
-export * from './navigation-prompt';
-export * from './notification-banner';
-export * from './orion-requestor';
-export * from './side-panel-manager';
-export * from './theme';
-export * from './workspace';
+export default TerraApplicationBase;
+// export * from './application-base';
+// export * from './application-error-boundary';
+// export * from './application-intl';
+// export * from './application-loading-overlay';
+// export * from './application-navigation';
+// export * from './breakpoints';
+// export * from './dislosure-manager';
+// export * from './modal-manager';
+// export * from './navigation-prompt';
+// export * from './notification-banner';
+// export * from './side-panel-manager';
+// export * from './theme';
+// export * from './workspace';

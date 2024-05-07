04f19ed7a55773674bc5d3819408383bb8631223
Saad <38024451+sdadn@users.noreply.github.com>
2024-05-07 12:32:23 -0500

    updated plugin

--- packages/terra-dev-site/src/webpack/plugin/resolve/LocalSubpathExportsResolverPlugin.js
+++ packages/terra-dev-site/src/webpack/plugin/resolve/LocalSubpathExportsResolverPlugin.js
@@ -11,40 +11,39 @@ const AliasPlugin = require('enhanced-resolve/lib/AliasPlugin');
 class LocalSubpathExportsResolverPlugin {
   constructor({ rootDirectories = [process.cwd()] } = {}) {
 
-    const result = rootDirectories.reduce((acc, root) => acc.concat(glob.sync(`${root}/package.json`).map((rootPath) => {
+    const result = rootDirectories.reduce((acc, root) => acc.concat(glob.sync(`${root}/package.json`))
+      .filter((rootPath)=>JSON.parse(fs.readFileSync(rootPath)).exports)
+      .map((rootPath) => {
       const dirname = path.dirname(rootPath);
       const packageName = JSON.parse(fs.readFileSync(rootPath)).name;
 
       const exports = JSON.parse(fs.readFileSync(rootPath)).exports
-      if(exports) {
-        console.log(dirname);
-        console.log(exports);
 
       const subpathAliases = [];
 
 
         for (const [key, value] of Object.entries(exports)) {
+
+          if(key.includes('*'))
+            continue;
         
+          let alias = value.replace('./', `${dirname}/`);
+          alias = alias.replace('/index.js', '')
           subpathAliases.push({
             name: key.replace('./', `${packageName}/`),
             onlyModule: false,
-            alias: value.replace('./', `${dirname}/`)
+            alias
           })
   
-          console.log(`${key}: ${value}`);
         }
   
         return subpathAliases;
-      }
 
-      return {}
 
-    })),
+    }),
     []);
 
     this.alias = result.flat()
-    console.log(this.alias);
-    // this.alias
   }
 
   apply(resolver) {
